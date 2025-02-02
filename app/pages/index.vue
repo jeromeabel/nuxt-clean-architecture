ab3dabf8cca7b03f528809abdad216d7d45162ba
Jérôme Abel <contact@jeromeabel.net>
2025-02-02 12:40:01 +0100

    style: add grid layout to the home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,16 @@
 <template>
-  <main class="container mx-auto grid items-center place-items-center h-screen">
-    <div class="w-fit border rounded p-8 shadow-lg">
-      <h1
-        class="text-5xl font-bold"
-      >
-        Nuxt Clean Architecture
-      </h1>
-    </div>
-  </main>
+  <div class="h-screen grid grid-rows-[auto_1fr]">
+    <header>
+      <VersionBannerStart />
+    </header>
+    <main class="container mx-auto grid items-center place-items-center">
+      <div class="w-fit border rounded p-8 shadow-lg">
+        <h1
+          class="text-5xl font-bold"
+        >
+          Nuxt Clean Architecture
+        </h1>
+      </div>
+    </main>
+  </div>
 </template>
