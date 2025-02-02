0175e530001c550446ef229483adea3024706348
Jérôme Abel <contact@jeromeabel.net>
2025-02-02 12:39:41 +0100

    feat: add VersionBannerStart

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 import { defineNuxtConfig } from 'nuxt/config'
-
+import pkg from './package.json'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
@@ -11,8 +11,7 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   runtimeConfig: {
     public: {
-      SUPABASE_URL: process.env.SUPABASE_URL,
-      SUPABASE_KEY: process.env.SUPABASE_KEY,
+      version: pkg.version,
     },
   },
   future: {
@@ -21,10 +20,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   eslint: {
     config: { stylistic: true },
-  },
-  supabase: {
-    url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_KEY,
-    redirect: false,
   },
 })
